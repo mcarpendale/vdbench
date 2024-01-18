5e5b218c91e478c3e1bb2312345d460a078d0041
Mike Carpendale <84347795+mcarpendale@users.noreply.github.com>
2024-01-18 10:57:52 +1000

    Update vdbench-jippers.sh

--- vdbench-jippers.sh
+++ vdbench-jippers.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# I use this with azure cli 
+# 1st base64 ./vdbench-jippers.sh > vdbench-jippers.txt
+# then add --custom-data vdbench-jippers.txt to az vm create \
 sudo apt-get -y update
 sudo apt-get -y install default-jdk
 sudo apt-get -y install unzip
-
-curl -O https://raw.githubusercontent.com/mcarpendale/vdbench/main/vdbench-jippers.sh
-chmod +x vdbench-jippers.sh
 curl -O https://raw.githubusercontent.com/mcarpendale/vdbench/main/post-deploy-create-disk-partition.sh
 chmod +x post-deploy-create-disk-partition.sh
+./post-deploy-create-disk-partition.sh
 mkdir vdbench
 cd vdbench
 #curl -O https://raw.githubusercontent.com/mcarpendale/vdbench/main/run_32K_70R.vdb
